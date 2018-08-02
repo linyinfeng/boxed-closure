0a8396529001e77f8142394cf14936282c60a367
Lin Yinfeng <lin.yinfeng@outlook.com>
2018-08-02 11:45:00 +0800

    Rewrite docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 #![feature(fn_traits, unboxed_closures)]
 
+//! Explain how closure works and provide a clear closure implementation with macro.
+//!
+//! # Example:
+//! ```
+//! # #[macro_use] extern crate closure;
+//! let mut s = String::from("Hello");
+//! {
+//!     let mut append = closure! {
+//!         ref mut [s: &mut String = &mut s,] (c: char,) -> usize {
+//!             s.push(c);
+//!             println!("{}", s);
+//!             s.len()
+//!         }
+//!     };
+//!     assert_eq!(append('!'), 6);
+//!     assert_eq!(append('!'), 7);
+//! }
+//! assert_eq!(s, "Hello!!");
+//! ```
+
 mod move_closure;
 mod ref_mut_closure;
 mod ref_closure;
@@ -8,86 +28,36 @@ pub use move_closure::MoveClosure;
 pub use ref_closure::RefClosure;
 pub use ref_mut_closure::RefMutClosure;
 
-/// Easily crate a closure
-/// # Example:
-/// ```rust
-/// let mut s = String::new("Hello")
-/// let mut append = closure! {
-///     ref mut [s: &mut String = &mut s] (c: char) -> () {
-///         s.push(c);
-///         println!("{}", s);
-///     }
-/// }
-/// append();
-/// append();
-/// ```
+/// A macro for writing closures easily
 #[macro_export] macro_rules! closure {
     (move [$($env_name:ident: $env_type:ty = $env_exp:expr,)*]
-        ($($arg_name:ident: $arg_type:ty,)*) -> $out:ty $body:block) => {
-        {
-            let env = ($($env_exp,)*);
-            fn f(env: ($($env_type,)*), args: ($($arg_type,)*)) -> $out {
-                let ($($env_name,)*) = env;
-                let ($($arg_name,)*) = args;
-                $body
-            }
-            $crate::MoveClosure::new(env, f)
-        }
-    };
-    (ref mut [$($env_name:ident: $env_type:ty = $env_exp:expr,)*]
-        ($($arg_name:ident: $arg_type:ty,)*) -> $out:ty $body:block) => {
-        {
-            let env = ($($env_exp,)*);
-            fn f(env: &mut ($($env_type,)*), args: ($($arg_type,)*)) -> $out {
-                let ($($env_name,)*) = env;
-                let ($($arg_name,)*) = args;
-                $body
-            }
-            $crate::RefMutClosure::new(env, f)
-        }
-    };
-    (ref [$($env_name:ident: $env_type:ty = $env_exp:expr,)*]
-        ($($arg_name:ident: $arg_type:ty,)*) -> $out:ty $body:block) => {
-        {
-            let env = ($($env_exp,)*);
-            fn f(env: &($($env_type,)*), args: ($($arg_type,)*)) -> $out {
-                let ($($env_name,)*) = env;
-                let ($($arg_name,)*) = args;
-                $body
-            }
-            $crate::RefClosure::new(env, f)
-        }
-    };
-}
-
-#[cfg(test)] 
-mod tests {
-    use super::*;
-    use std::any::TypeId;
-    fn type_id_of<T: ?Sized + 'static>(_: &T) -> TypeId {
-        TypeId::of::<T>()
-    }
-
-    #[test]
-    fn test_empty_move_closure() {
-        let c = closure!(move [] () -> () {});
-        assert_eq!(type_id_of(&c), TypeId::of::<MoveClosure<(), (), ()>>());
-        assert_eq!(c(), ());
-    }
-
-    #[test]
-    fn test_empty_ref_mut_closure() {
-        let mut c = closure!(ref mut [] () -> () {});
-        assert_eq!(type_id_of(&c), TypeId::of::<RefMutClosure<(), (), ()>>());
-        assert_eq!(c(), ());
-        assert_eq!(c(), ());
-    }
-
-    #[test]
-    fn test_empty_ref_closure() {
-        let c = closure!(ref [] () -> () {});
-        assert_eq!(type_id_of(&c), TypeId::of::<RefClosure<(), (), ()>>());
-        assert_eq!(c(), ());
-        assert_eq!(c(), ());
-    }
+        ($($arg_name:ident: $arg_type:ty,)*) -> $out:ty $body:block) => ({
+        fn f(($($env_name,)*): ($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) -> $out $body
+        $crate::MoveClosure::new(($($env_exp,)*), f)
+    });
+    (move [$($env_name:ident: $env_type:ty = $env_exp:expr,)*]
+        ($($arg_name:ident: $arg_type:ty,)*) $body:block) => ({
+        fn f(($($env_name,)*): ($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) $body
+        $crate::MoveClosure::new(($($env_exp,)*), f)
+    });
+    (ref mut [$($env_name:ident: $env_type:ty = $env_exp:expr),*,]
+        ($($arg_name:ident: $arg_type:ty),*,) -> $out:ty $body:block) => ({
+        fn f(($($env_name,)*): &mut ($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) -> $out $body
+        $crate::RefMutClosure::new(($($env_exp,)*), f)
+    });
+    (ref mut [$($env_name:ident: $env_type:ty = $env_exp:expr),*,]
+        ($($arg_name:ident: $arg_type:ty),*,) $body:block) => ({
+        fn f(($($env_name,)*): &mut ($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) $body
+        $crate::RefMutClosure::new(($($env_exp,)*), f)
+    });
+    (ref [$($env_name:ident: $env_type:ty = $env_exp:expr),*,]
+        ($($arg_name:ident: $arg_type:ty),*,) -> $out:ty $body:block) => ({
+        fn f(($($env_name,)*): &($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) -> $out $body
+        $crate::RefClosure::new(($($env_exp,)*), f)
+    });
+    (ref [$($env_name:ident: $env_type:ty = $env_exp:expr),*,]
+        ($($arg_name:ident: $arg_type:ty),*,) $body:block) => ({
+        fn f(($($env_name,)*): &($($env_type,)*), ($($arg_name,)*): ($($arg_type,)*)) $body
+        $crate::RefClosure::new(($($env_exp,)*), f)
+    });
 }
